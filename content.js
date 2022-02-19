3b48efe127a0e1a39b013977340f805208e09306
Griffin Baxter <griffinrbaxter@gmail.com>
2022-02-19 13:01:44 +1300

    Cleaned/reorganised code and removed "then" statements in favour of exclusively using "await" for async functions.

--- content.js
+++ content.js
@@ -17,49 +17,53 @@ let currentTimeout;
 
 
 /**
+ * Main function, sets listener and initialises video speed.
+ */
+function main() {
+    setAndHandleKeyDown();
+    if (videoExists) {
+        setSpeedFromStorage();
+    }
+}
+
+/**
+ * Sets the key down listener and handles the event.
+ */
+function setAndHandleKeyDown() {
+    document.addEventListener("keydown", async function (event) {
+        if (videoExists) {
+            await handleKeyDown(event);
+            showSpeedOverlay();
+        }
+    });
+}
+
+/**
  * Checks whether a video exists with a playback rate parameter within the current page.
- *
- * @returns {boolean} True if a video exists with a playback rate parameter, false otherwise.
+ * @returns {boolean} True if a video exists, false otherwise.
  */
 function videoExists() {
     const videoElements = document.getElementsByTagName("video");
-    if (videoElements.length > 0) {
-        return videoElements[0].playbackRate !== undefined;
-    }
-    return false;
+    return videoElements.length > 0
+        ? videoElements[0].playbackRate !== undefined
+        : false;
 }
-
 
 /**
- * Removes the video speed overlay from the page if it exists.
+ * Handles any key down events, which calls the respective function depending on whether one of the set video speed keys
+ * is pressed.
  */
-function removeOverlay() {
-    if (document.getElementById("video-speed-overlay") != null) {
-        document.getElementById("video-speed-overlay").remove();
+async function handleKeyDown(event) {
+    if (SPEED_DOWN_KEYS.includes(event.key)) {
+        await speedDown();
+    } else if (SPEED_UP_KEYS.includes(event.key)) {
+        await speedUp();
+    } else if (RESET_SPEED_KEYS.includes(event.key)) {
+        await setSpeed(DEFAULT_SPEED);
+    } else if (SHOW_OVERLAY_KEYS.includes(event.key)) {
+        setSpeedFromStorage();
     }
 }
-
-
-/**
- * Shows the video speed overlay on the current page, using the current video speed. This is done by clearing the
- * current timeout and removing the existing overlay if it exists, then adding the current video speed overlay as a div
- * with a timeout set by OVERLAY_TIMEOUT_MS.
- */
-function showSpeedOverlay() {
-    clearTimeout(currentTimeout);
-    removeOverlay();
-
-    const overlayDiv = document.createElement("div");
-    overlayDiv.setAttribute("id", "video-speed-overlay");
-    document.body.insertBefore(overlayDiv, document.body.firstChild);
-    document.getElementById("video-speed-overlay").innerText =
-        document.getElementsByTagName("video")[0].playbackRate.toString();
-
-    currentTimeout = setTimeout(() => {
-        removeOverlay();
-    }, OVERLAY_TIMEOUT_MS);
-}
-
 
 /**
  * Speeds the video down by the increment set by SPEED_INCREMENT to a minimum of MIN_SPEED.
@@ -69,7 +73,7 @@ async function speedDown() {
     if (originalSpeed >= MIN_SPEED + SPEED_INCREMENT) {
         document.getElementsByTagName("video")[0].playbackRate -= SPEED_INCREMENT;
         await chrome.storage.local.set(
-            {videoSpeed: document.getElementsByTagName("video")[0].playbackRate}
+            { videoSpeed: document.getElementsByTagName("video")[0].playbackRate }
         );
     }
 }
@@ -82,15 +86,37 @@ async function speedUp() {
     if (originalSpeed <= MAX_SPEED - SPEED_INCREMENT) {
         document.getElementsByTagName("video")[0].playbackRate += SPEED_INCREMENT;
         await chrome.storage.local.set(
-            {videoSpeed: document.getElementsByTagName("video")[0].playbackRate}
+            { videoSpeed: document.getElementsByTagName("video")[0].playbackRate }
         );
     }
 }
 
+/**
+ * Retrieves the "videoSpeed" key from the local chrome.storage API, and sets the video speed according to the key's
+ * value. If the key is not set or not within the max and min boundaries, it is set to DEFAULT_SPEED.
+ */
+function setSpeedFromStorage() {
+    chrome.storage.local.get(["videoSpeed"], async function (result) {
+        if (isSpeedWithinBoundaries(result.videoSpeed)) {
+            await setSpeed(result.videoSpeed);
+        } else {
+            await setSpeed(DEFAULT_SPEED);
+            await chrome.storage.local.set({ videoSpeed: DEFAULT_SPEED });
+        }
+    });
+}
+
+/**
+ * Checks if the given video speed is within the max and min boundaries.
+ * @param videoSpeed Video speed number.
+ * @returns {boolean} True if video speed is within boundaries, false otherwise.
+ */
+function isSpeedWithinBoundaries(videoSpeed) {
+    return !isNaN(videoSpeed) && videoSpeed <= MAX_SPEED && videoSpeed >= MIN_SPEED;
+}
 
 /**
  * Sets the video speed to the given value.
- *
  * @param speed Number to set the video speed to.
  */
 async function setSpeed(speed) {
@@ -98,58 +124,42 @@ async function setSpeed(speed) {
     await chrome.storage.local.set({videoSpeed: speed});
 }
 
+/**
+ * Shows the video speed overlay on the current page, using the current video speed.
+ */
+function showSpeedOverlay() {
+    clearTimeout(currentTimeout);
+    removeOverlay();
+    setOverlayInDocument();
+    setOverlayTimeout();
+}
 
 /**
- * Retrieves the "videoSpeed" key from the chrome.storage API, and sets the video speed according to the key's value.
- * If the key is not set, it is set to DEFAULT_SPEED.
- *
- * @param showOverlay Determines whether the speed overlay should be shown after completion.
+ * Creates and sets the video speed overlay as a div.
  */
-function setSpeedFromStorage(showOverlay) {
-    chrome.storage.local.get(["videoSpeed"], async function (result) {
-        if (!isNaN(result.videoSpeed)) {
-            if (result.videoSpeed < MIN_SPEED) {
-                await setSpeed(MIN_SPEED);
-                await chrome.storage.local.set({videoSpeed: MIN_SPEED});
-            } else if (result.videoSpeed > MAX_SPEED) {
-                await setSpeed(MAX_SPEED);
-                await chrome.storage.local.set({videoSpeed: MAX_SPEED});
-            } else {
-                await setSpeed(result.videoSpeed);
-            }
-        } else if (isNaN(result.videoSpeed)) {
-            await setSpeed(DEFAULT_SPEED);
-            await chrome.storage.local.set({videoSpeed: DEFAULT_SPEED});
-        }
-        if (showOverlay) {
-            showSpeedOverlay();
-        }
-    });
+function setOverlayInDocument() {
+    const overlayDiv = document.createElement("div");
+    overlayDiv.setAttribute("id", "video-speed-overlay");
+    document.body.insertBefore(overlayDiv, document.body.firstChild);
+    document.getElementById("video-speed-overlay").innerText =
+        document.getElementsByTagName("video")[0].playbackRate.toString();
 }
-
 
 /**
- * Handles any keydown events, which calls the respective function depending on whether one of the set video speed keys
- * is pressed and shows the speed overlay.
+ * Sets a timeout period for the overlay to stay in view.
  */
-async function handleKeyDown(event) {
-    if (SPEED_DOWN_KEYS.includes(event.key) && videoExists) {
-        speedDown().then(() => showSpeedOverlay());
-    } else if (SPEED_UP_KEYS.includes(event.key) && videoExists) {
-        speedUp().then(() => showSpeedOverlay());
-    } else if (RESET_SPEED_KEYS.includes(event.key) && videoExists) {
-        setSpeed(DEFAULT_SPEED).then(() => showSpeedOverlay());
-    } else if (SHOW_OVERLAY_KEYS.includes(event.key) && videoExists) {
-        setSpeedFromStorage(true);
-    }
+function setOverlayTimeout() {
+    currentTimeout = setTimeout(() => {
+        removeOverlay();
+    }, OVERLAY_TIMEOUT_MS);
+}
+
+/**
+ * Removes the video speed overlay from view.
+ */
+function removeOverlay() {
+    document.getElementById("video-speed-overlay")?.remove();
 }
 
 
-document.addEventListener("keydown", async function (event) {
-    await handleKeyDown(event);
-});
-
-
-if (videoExists) {
-    setSpeedFromStorage(false);
-}
+main();
